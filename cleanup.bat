364526ee97cd39e7fcc245aa3976f679a390c232
Lee <71296232+leeprky@users.noreply.github.com>
2021-08-03 14:58:12 +0100

    Delete cleanup.bat

--- cleanup.bat
+++
@@ -1,3 +0,0 @@
-del /f /q "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup\GlasscordHelper*.*"
-for /d %%d in ("%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup\GlasscordHelper*.*") do rmdir /s /q "%%d"
-mkdir %USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup\"

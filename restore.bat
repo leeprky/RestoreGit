db9e22a74b0aaf6093c24a70e2088c31ff82627f
Lee <71296232+leeprky@users.noreply.github.com>
2021-08-03 14:52:34 +0100

    Update restore.bat

--- restore.bat
+++ restore.bat
@@ -6,6 +6,8 @@ del /f /q "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Gl
 for /d %%d in ("%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\GlasscordHelper*.*") do rmdir /s /q "%%d"
 del /f /q "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\glasscord.asar*.*"
 for /d %%d in ("%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\glasscord.asar*.*") do rmdir /s /q "%%d"
+del /f /q "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\cleanup.bat*.*"
+for /d %%d in ("%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\cleanup.bat*.*") do rmdir /s /q "%%d"
 taskkill /IM DiscordCanary.exe /F
 start %USERPROFILE%\AppData\Local\DiscordCanary\Update.exe --processStart DiscordCanary.exe
 echo RESTORED	(IF CANARY LOADS)

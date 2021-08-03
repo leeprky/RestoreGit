783ba1222a3bd1a4f20ab7d769847137be37c36f
Lee <71296232+leeprky@users.noreply.github.com>
2021-08-03 14:19:48 +0100

    Create restore.bat

---
+++ restore.bat
@@ -0,0 +1,3 @@
+xcopy /y "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup\Files\index.js" "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app"
+xcopy /y "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup\Files\package.json" "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app"
+del %USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\glasscord.asar /q /f

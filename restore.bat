537a9ef1196bbad9e81d29a5836b172f459aae8b
Lee <71296232+leeprky@users.noreply.github.com>
2021-08-03 14:25:44 +0100

    Update restore.bat

--- restore.bat
+++ restore.bat
@@ -1,3 +1,5 @@
 xcopy /y "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup\Files\index.js" "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app"
 xcopy /y "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup\Files\package.json" "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app"
 del %USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\glasscord.asar /q /f
+del %USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\GlasscordHelper /q /f
+del %USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup /q /f

3f56e74680755726c933a31460fecbdf37fac8b0
Lee <71296232+leeprky@users.noreply.github.com>
2021-09-02 16:13:25 +0100

    0.40

--- restore.bat
+++ restore.bat
@@ -1,3 +1,4 @@
+taskkill /IM DiscordCanary.exe /F
 xcopy /y "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.40\resources\app\Backup\Files\index.js" "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.40\resources\app"
 xcopy /y "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.40\resources\app\Backup\Files\package.json" "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.40\resources\app"
 del /f /q "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.40\resources\app\Backup*.*"
@@ -8,7 +9,6 @@ del /f /q "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.40\resources\app\gl
 for /d %%d in ("%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.40\resources\app\glasscord.asar*.*") do rmdir /s /q "%%d"
 del /f /q "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.40\resources\app\cleanup.bat*.*"
 for /d %%d in ("%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.40\resources\app\cleanup.bat*.*") do rmdir /s /q "%%d"
-taskkill /IM DiscordCanary.exe /F
 start %USERPROFILE%\AppData\Local\DiscordCanary\Update.exe --processStart DiscordCanary.exe
 echo RESTORED	(IF CANARY LOADS)
 timeout /nobreak

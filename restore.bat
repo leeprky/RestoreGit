dd4abdf5cbf92451d741000471cf722e8728d2ca
Lee <71296232+leeprky@users.noreply.github.com>
2021-10-15 11:41:33 +0100

    Update restore.bat

--- restore.bat
+++ restore.bat
@@ -1,14 +1,14 @@
 taskkill /IM DiscordCanary.exe /F
-xcopy /y "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.40\resources\app\Backup\Files\index.js" "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.40\resources\app"
-xcopy /y "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.40\resources\app\Backup\Files\package.json" "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.40\resources\app"
-del /f /q "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.40\resources\app\Backup*.*"
-for /d %%d in ("%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.40\resources\app\Backup*.*") do rmdir /s /q "%%d"
-del /f /q "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.40\resources\app\GlasscordHelper*.*"
-for /d %%d in ("%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.40\resources\app\GlasscordHelper*.*") do rmdir /s /q "%%d"
-del /f /q "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.40\resources\app\glasscord.asar*.*"
-for /d %%d in ("%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.40\resources\app\glasscord.asar*.*") do rmdir /s /q "%%d"
-del /f /q "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.40\resources\app\cleanup.bat*.*"
-for /d %%d in ("%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.40\resources\app\cleanup.bat*.*") do rmdir /s /q "%%d"
+xcopy /y "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.42\resources\app\Backup\Files\index.js" "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.42\resources\app"
+xcopy /y "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.42\resources\app\Backup\Files\package.json" "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.42\resources\app"
+del /f /q "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.42\resources\app\Backup*.*"
+for /d %%d in ("%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.42\resources\app\Backup*.*") do rmdir /s /q "%%d"
+del /f /q "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.42\resources\app\GlasscordHelper*.*"
+for /d %%d in ("%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.42\resources\app\GlasscordHelper*.*") do rmdir /s /q "%%d"
+del /f /q "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.42\resources\app\glasscord.asar*.*"
+for /d %%d in ("%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.42\resources\app\glasscord.asar*.*") do rmdir /s /q "%%d"
+del /f /q "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.42\resources\app\cleanup.bat*.*"
+for /d %%d in ("%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.42\resources\app\cleanup.bat*.*") do rmdir /s /q "%%d"
 start %USERPROFILE%\AppData\Local\DiscordCanary\Update.exe --processStart DiscordCanary.exe
 echo RESTORED	(IF CANARY LOADS)
 timeout /nobreak
